3c614ec03ba65e1af8df50f18df70277b78b471d
Marko Grešak <gresak.marko@gmail.com>
2017-09-05 17:55:47 +0200

    Implement handling for repos with submodule setting set to true

--- src/clone-git-repository.js
+++ src/clone-git-repository.js
@@ -30,17 +30,41 @@ function checkIfRepositoryExists(repoPath) {
   })
 }
 
-function cloneRepository(repoUrl, repoPath, cloneOptions) {
+function processSubmodule(repoConfig, cloneOptions, repo, submoduleName) {
+  log(`Lookup for submodule ${submoduleName} for repo ${repoConfig.name}`)
+  return Git.Submodule.lookup(repo, submoduleName).then(submodule => {
+    log(`Setting up submodule ${submodule.path()} for repo ${repoConfig.name}`)
+
+    // Adapted from https://github.com/nodegit/nodegit/issues/560#issuecomment-127983557
+    return submodule.init(1)
+      .then(() => log(`Submodule init for repo ${repoConfig.name} was successful`))
+      .then(() => submodule.update(0, new Git.SubmoduleUpdateOptions()))
+      .catch(() => process.exit(1))
+      .then(() => log(`Submodule update for repo ${repoConfig.name} was successful`))
+      .catch(err => error(`Submodule setup for repo ${repoConfig.name} failed: ${err.toString()}\n${err.stack}`))
+  })
+}
+
+function cloneRepository(repoUrl, repoPath, cloneOptions, repoConfig) {
   log(`clone repository from url ${repoUrl} to ${repoPath}`)
   const cloneStartTime = process.uptime() * 1000
   return Git.Clone(repoUrl, repoPath, cloneOptions)
     .then(repo => {
       const cloneEndTime = process.uptime() * 1000
       log(`clone ${repoUrl}: ${cloneEndTime - cloneStartTime}ms`)
+      if (repoConfig.submodule) {
+        log(`The repo ${repoConfig.name} is configured with submodule, setting up submodules...`)
+        return repo.getSubmoduleNames()
+          .then(names => Promise.all(names.map(processSubmodule.bind(null, repoConfig, cloneOptions, repo))))
+          .then(() => log(`Submodules for repo ${repoConfig.name} was successfully configured`))
+          .catch(err => error(`An error occured while configuring submodules for ${repoConfig.name}, ${err.toString()}\n${err.stack}`))
+          .then(() => repo)
+      }
+
       return repo
     })
     .catch(err => {
-      error(`git clone for repo '${repoUrl}' exited with error: ${err}`)
+      error(`git clone for repo '${repoUrl}' exited with error: ${err.toString()}\n${err.stack}`)
       // throw error to propagate it to next .catch
       throw err
     })
@@ -69,7 +93,7 @@ function cloneGitRepository(githubToken, repoConfig) {
       // repository already exists, skip clone
       return Git.Repository.open(repoPath)
     }
-    return cloneRepository(repoUrl, repoPath, cloneOptions)
+    return cloneRepository(repoUrl, repoPath, cloneOptions, repoConfig)
   })
 }
 
